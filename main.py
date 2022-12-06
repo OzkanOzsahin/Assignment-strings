fe923d24edb2fe8ee0fac5b8d7760d1a3da38738
Ozkan Ozsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2022-12-06 13:54:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,51 +3,43 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-
-
-#Part 1 
-#1 who scored?
-player_0 = "Ruud Gullit"
-player_1 = "Marco van Basten"
-print (player_0, player_1)
-
-#2 What minute?
+#Part 1
+#1
+player_0 = 'Ruud Gullit'
+player_1 = 'Marco van Basten'
+#2
 goal_0 = 32
 goal_1 = 54
-print (goal_0, goal_1)
-
-#3 Scorers 
-# using + operator create string
-# <scorer_name> <when_they_scored>, <scorer_name> <when_they_scored>
-scorers = (f"{player_0} {goal_0}, {player_1} {goal_1}")
-report = f"{player_0} scored in the {goal_0}nd minute\n{player_1} scored in {goal_1}th minute"
+#3
+scorers = f'{player_0} {goal_0}, {player_1} {goal_1}'
+print(scorers)
+#4 using f strings
+report = f'{player_0} scored in the {goal_0}nd minute\n{player_1} scored in the {goal_1}th minute'
 print(report)
-
-#4 use f strings to create single string who scored when..
-
 
 
 #Part 2
 #1
-player = 'Ruud Gullit'
+player = player_0
 #2
-first_name = player[:player.find(' '):]
-#3
-last_name = player[:player.find(' '):]
-
+find_space = player.find(" ")
+first_name = player[:find_space]
+print(first_name)
 first_name_len = len(first_name)
-last_name_len = len(last_name)
+#3
+last_name = player[find_space:]
+last_name_len = len(last_name)-1
+print(last_name_len)
 #4
-name_short = (player[0] + '. ' + last_name)
-print (name_short)
+initial = player[0] + '.'
+name_short = initial + last_name
+print(name_short)
 
-#5 
-yell = first_name + '! '
-chant = yell *(first_name_len)
-chant = chant.rstrip(chant[-1])
+#5
+chant = ((first_name + '! ') * first_name_len)[:-1] 
 print(chant)
 
-good_chant = (chant[-1] != ' ')
-print (good_chant)
-
+#6
+good_chant = chant[len(chant)-1] != " "
+print(good_chant)
 
